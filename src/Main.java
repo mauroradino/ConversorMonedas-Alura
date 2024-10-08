244e57743b696da02a7d78fe38ded7137b9ad815
mauroradino <mauroradino2003@gmail.com>
2024-10-08 17:16:34 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,46 @@
+import com.google.gson.Gson;
+
+import java.awt.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner inputUsuario = new Scanner(System.in);
+
+        boolean condicionSalida = true;
+        while(condicionSalida){
+        System.out.println("Ingrese la moneda de entrada: (ARS/ USD / VES / GBP / BRL)");
+            String monedaIn = inputUsuario.nextLine();
+        System.out.println("Ingrese la moneda de salida: (ARS/ USD / VES / GBP / BRL)");
+        String monedaOut = inputUsuario.nextLine();
+        System.out.println("Ingrese el monto a convertir");
+        int cantidad = inputUsuario.nextInt();
+            inputUsuario.nextLine();
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://v6.exchangerate-api.com/v6/77d8033cfefb98d785a4551c/latest/"+monedaIn.toUpperCase()))
+                .build();
+
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+        Gson gson = new Gson();
+        Respuesta respuesta = gson.fromJson(response.body(), Respuesta.class);
+        double tasa = respuesta.getConversion_rates().get(monedaOut.toUpperCase());
+        double valor = cantidad * tasa;
+        System.out.println("El valor es de: " + valor);
+            System.out.println("Ingrese '0' para salir o '1' para seguir convirtiendo");
+            String salida = inputUsuario.nextLine();
+            if(salida.equals("0")){
+                condicionSalida = false;
+            }
+        }
     }
 }
---
+++ src/Respuesta.java
@@ -0,0 +1,18 @@
+import java.util.Map;
+
+public class Respuesta {
+    String result;
+    String documentation;
+    String terms_of_use;
+    String time_last_update_unix;
+    String time_last_update_utc;
+    String time_next_update_unix;
+    String time_next_update_utc;
+    String base_code;
+    Map<String, Double> conversion_rates;
+
+
+    public Map<String, Double> getConversion_rates() {
+        return conversion_rates;
+    }
+}
